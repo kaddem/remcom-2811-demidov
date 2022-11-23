4750c772427d5393567cb32be822c78f77bad369
Kirill Demidov <kademidov@gmail.com>
2022-11-23 18:45:13 +0300

    адаптив главной с занятия

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,16 @@
 console.log('script work!');
+
+let myName;
+console.log(myName);
+
+myName = 'Kirill';
+console.log(myName);
+
+myName = 'Katja';
+console.log(myName);
+
+
+const PiNumber = 3.141592;
+console.log(PiNumber);
+
+
